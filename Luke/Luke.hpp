cc51004968699a675df621a5dcc525e58ed93760
Matthias Dörfelt <hi@mokafolio.de>
2017-10-03 15:59:59 -0700

    added Luke.hpp header to include the whole library

---
+++ Luke/Luke.hpp
@@ -0,0 +1,10 @@
+#ifndef LUKE_LUKE_HPP
+#define LUKE_LUKE_HPP
+
+#include <Luke/Display.hpp>
+#include <Luke/KeyEvents.hpp>
+#include <Luke/MouseEvents.hpp>
+#include <Luke/Window.hpp>
+#include <Luke/WindowEvents.hpp>
+
+#endif //LUKE_LUKE_HPP
